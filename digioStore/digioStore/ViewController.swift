bc3b1c54c8832ebece15d6cc03be0a705f1de796
raduchiu <raduchiu.amaral@shopper.com.br>
2024-05-26 19:26:15 -0300

    create project

---
+++ digioStore/digioStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  digioStore
+//
+//  Created by Raduchiu Amaral on 26/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ digioStore/digioStoreUITests/digioStoreUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  digioStoreUITestsLaunchTests.swift
+//  digioStoreUITests
+//
+//  Created by Raduchiu Amaral on 26/05/24.
+//
+
+import XCTest
+
+final class digioStoreUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
